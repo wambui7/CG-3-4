e9168a4db6671e3cfc2e0299dc91827da09a3052
wambui7 <shiko.sheila@gmail.com>
2018-07-26 20:34:11 -0500

    commit

--- maths/Program.cs
+++ maths/Program.cs
@@ -6,7 +6,54 @@ namespace maths
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("This programme is for calculations where you can add subtract divide 2 numbers");
+            string firstnumber = Console.ReadLine();
+            // i will now turn the number from a string to integer
+
+            int firstnumb = int.Parse(firstnumber);
+            Console.WriteLine("the first number" + firstnumb + "thesecondnumber" + ".");
+
+            // will input the second number and change itfrom a string toan integer
+
+            string secondnumber = Console.ReadLine();
+            int secondnumb = int.Parse(secondnumber);
+            Console.WriteLine("numbers are" + firstnumb + "and " + secondnumb + ".");
+            Console.Write("would love to add, subtract, multiply and divide");
+
+            // i will use the maths operator and find a value on each output 
+
+            string mathsoperator = Console.ReadLine();
+            int lastnumb = 0;
+
+            if (mathsoperator == "add")
+            {
+
+                lastnumb = firstnumb + secondnumb;
+            }
+
+            else if (mathsoperator == "subtraction")
+            {
+
+                lastnumb = firstnumb - secondnumb;
+            }
+
+
+            else if (mathsoperator == "multiplication")
+            {
+
+                lastnumb = firstnumb * secondnumb;
+            }
+            else if (mathsoperator == "divide")
+            {
+                lastnumb = firstnumb / secondnumb;
+            }
+            else
+            {
+                Console.WriteLine("incorrect");
+            }
+            Console.WriteLine(".");
+            Console.ReadLine();
         }
     }
 }
